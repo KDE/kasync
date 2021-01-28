5707cf97f1273262383dfd9d16801c86d383a23d
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-01-28 15:50:43 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification

--- src/job_impl.h
+++ src/job_impl.h
@@ -1,21 +1,10 @@
 /*
- * Copyright 2014 - 2015 Daniel Vrátil <dvratil@redhat.com>
- * Copyright 2015 - 2016 Daniel Vrátil <dvratil@kde.org>
- * Copyright 2016  Christian Mollekopf <mollekopf@kolabsystems.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public License
- * along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
+    SPDX-FileCopyrightText: 2014-2015 Daniel Vrátil <dvratil@redhat.com>
+    SPDX-FileCopyrightText: 2015-2016 Daniel Vrátil <dvratil@kde.org>
+    SPDX-FileCopyrightText: 2016 Christian Mollekopf <mollekopf@kolabsystems.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 
 #ifndef KASYNC_JOB_IMPL_H
 #define KASYNC_JOB_IMPL_H
